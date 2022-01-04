126672f481080be9645af5f5af2c73afd8f3c65e
Anoel17 <57414276+Anoel17@users.noreply.github.com>
2022-01-04 08:13:51 -0500

    Added category filter

--- src/main/java/com/project3/revtech/dao/ProductRepository.java
+++ src/main/java/com/project3/revtech/dao/ProductRepository.java
@@ -1,12 +1,17 @@
 package com.project3.revtech.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.project3.revtech.entity.Product;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
     //----- Product Custom JPA Queries ----------//
 
 
+	List<Product> findAllByProductCategory(String productCategory);
 }
