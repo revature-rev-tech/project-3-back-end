2676439a57f2e323236c380f9e9195485da1e26c
alara505 <alex.lara@revature.net>
2022-02-23 15:06:07 -0600

    Added some negative sad tests for the controllers. Additionally, removing all the code from AuthController

--- src/test/java/com/project3/revtech/controller/DiscountControllerTest.java
+++ src/test/java/com/project3/revtech/controller/DiscountControllerTest.java
@@ -1,4 +1,40 @@
 package com.project3.revtech.controller;
 
+import com.project3.revtech.controller.DiscountController;
+import com.project3.revtech.exception.ApplicationException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoProperties;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import com.project3.revtech.exception.GlobalExceptionHandler;
+import com.project3.revtech.dao.UserRepository;
+import com.project3.revtech.entity.User;
+import com.project3.revtech.entity.Role;
+import com.project3.revtech.entity.ERole;
+import com.project3.revtech.request.LoginRequest;
+import com.project3.revtech.request.SignupRequest;
+import com.project3.revtech.response.MessageResponse;
+import org.testng.annotations.Test;
+
 public class DiscountControllerTest {
+
+    @Autowired
+    private DiscountController discountController;
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @Test
+    public void contextLoads() throws Exception {
+        assertThat(discountController).isNull();
+    }
 }
--- src/test/java/com/project3/revtech/controller/ProductControllerTest.java
+++ src/test/java/com/project3/revtech/controller/ProductControllerTest.java
@@ -1,4 +1,52 @@
 package com.project3.revtech.controller;
 
+import com.project3.revtech.dao.ProductRepository;
+import com.project3.revtech.entity.Product;
+import com.project3.revtech.exception.ApplicationException;
+import com.project3.revtech.pojo.ProductPojo;
+import com.project3.revtech.service.ProductService;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class ProductControllerTest {
+
+    @Autowired
+    private ProductController controller;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void contextLoads() throws Exception {
+        assertThat(controller).isNotNull();
+    }
+
+    // User needs to be logged in to get product
+    // "Full authentication is required to access this resource","status":401
+    @Test
+    public void shouldReturnUnauthorizedNotLoggedIn() throws Exception {
+        this.mockMvc.perform(get("/products/getone/1")).andDo(print()).andExpect(status().isUnauthorized());
+    }
+
+    //"Full authentication is required to access this resource","status":401
+    @Test
+    public void shouldReturnGetAllUnauthorizedNotLoggedIn() throws Exception {
+        this.mockMvc.perform(get("/products/getall")).andDo(print()).andExpect(status().isUnauthorized());
+    }
+
+
 }
+
